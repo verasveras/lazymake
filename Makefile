544c191ffcff1a1e1fe87f2441e8ec43bc41919f
Lisa Veras <lisaveras@gmail.com>
2019-02-02 11:05:35 -0500

    parsing makefile and showing options works

---
+++ Makefile
@@ -0,0 +1,8 @@
+test:
+	echo "pets are nice"
+test2:
+	echo "hamsters are okay"
+test3:
+	echo "panko is a dog"
+tes4:
+	echo "zelda is a cat"
--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const parser = require('makefile-parser');
-const cliSelect = require('cli-select');
-const chalk = require('chalk');
+const parser = require("@kba/makefile-parser");
+const fs = require("fs");
+const cliSelect = require("cli-select");
+const chalk = require("chalk");
+
+// TODO take as command
+// absolute path to this folder and then you append the location of the makefile
+
+const makeFile = fs.readFileSync("./Makefile", "utf8");
+const parsedMake = parser(makeFile);
+
+const targets = [];
+const recipes = {};
+
+parsedMake.ast.forEach(target => {
+  const targetName = target.target;
+  targets.push(targetName);
+  recipes[targetName] = {
+    ...target
+  };
+  delete recipes[targetName].target;
+});
 
 const options = {
-    values: ['cats', 'dogs'],
-    valueRenderer: (value, selected)  => {
-        if (selected) return chalk.bgWhite(value);
-        return value;
-    },
+  values: targets,
+  valueRenderer: (value, selected) => {
+    if (selected) return chalk.bgWhite(value);
+    return value;
+  }
 };
 
-cliSelect(options)
-    .then(() => {
-        console.log('wow!!!');
-    });
+cliSelect(options).then(() => {
+  console.log("wow!!!");
+});

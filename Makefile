14f4f7ff83354d74ee17c5a5624334bd5292659b
verasveras <lisaveras@gmail.com>
2019-02-03 19:00:18 -0500

    ability to give command line arg for makefile location

--- Makefile
+++ Makefile
@@ -4,5 +4,5 @@ test2:
 	echo "hamsters are okay"
 test3:
 	echo "panko is a dog"
-tes4:
+test4:
 	echo "zelda is a cat"
--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const exec = require("child_process").exec;
 const parser = require("@kba/makefile-parser");
 const cliSelect = require("cli-select");
 const chalk = require("chalk");
+const path = require("path");
 
-// TODO take as command
-// absolute path to this folder and then you append the location of the makefile
+const args = process.argv.splice(2);
+const makefilePath = args[0] ? args[0] : "./Makefile";
+const resolvedPath = path.resolve(__dirname, makefilePath);
 
-const makeFile = fs.readFileSync("./Makefile", "utf8");
+const makeFile = fs.readFileSync(resolvedPath, "utf8");
 const parsedMake = parser(makeFile);
 
 const targets = [];
@@ -30,9 +32,10 @@ const options = {
   }
 };
 
-console.log("Select a makefile target to run");
+console.log("Select a makefile target to run.");
 cliSelect(options).then(response => {
   const { value } = response;
+  console.log(recipes[value]);
   exec(`make ${value}`, (err, stdout, stderr) => {
     if (err) {
       console.error(`exec error: ${err}`);
